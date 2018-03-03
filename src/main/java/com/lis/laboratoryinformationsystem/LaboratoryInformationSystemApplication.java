933f23c5b5055a5d3ab31b2ee49d1c7ac3edcd1f
Ferran Parareda <fredy87@gmail.com>
2018-03-03 12:36:15 +0100

    initial structure

---
+++ src/main/java/com/lis/laboratoryinformationsystem/LaboratoryInformationSystemApplication.java
@@ -0,0 +1,12 @@
+package com.lis.laboratoryinformationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaboratoryInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaboratoryInformationSystemApplication.class, args);
+	}
+}
